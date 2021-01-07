f98e9e5cad1b837684b0539e35035cf0c443422f
Maya Salcedo <68334235+maya-salcedo@users.noreply.github.com>
2021-01-07 22:08:59 +0200

    added codes to the function

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
 buttonColours = ["red", "blue", "green", "yellow"];
+gamePattern = [];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
-    gamePattern = [];
     gamePattern.push(randomChosenColour);
-};
+    console.log(randomChosenColour); 
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();  
+}
+
+
+
+
+
+
 
 
 
