aaae2fe5e13df0f719891d26bcb9e8003ffe6835
Maya Salcedo <68334235+maya-salcedo@users.noreply.github.com>
2021-01-08 21:49:35 +0200

    added functions

--- game.js
+++ game.js
@@ -1,17 +1,48 @@
 buttonColours = ["red", "blue", "green", "yellow"];
 gamePattern = [];
+userClickedPattern = [];
+var started = false; //to keep track if the game has started or not
+var level = 0;
+
+$(document).keydown(function(){   
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+
+});
+
+$(".btn").click(function (){ 
+    var userChosenColour = $(this).attr("id"); //to refer to the button object that triggered the click; this.id also works
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+}); 
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    console.log(randomChosenColour); 
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();  
+    console.log(gamePattern); 
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+      
 }
 
 
+function playSound (name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){ $("#" + currentColour).removeClass("pressed"); }, 100);
+}
 
 
 
