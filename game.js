1e2264a5414fe0cbd5f9eb7fb1299687b0c2b219
Maya Salcedo <68334235+maya-salcedo@users.noreply.github.com>
2021-01-11 09:30:46 +0200

    added functions

--- game.js
+++ game.js
@@ -8,20 +8,20 @@ $(document).keydown(function(){
     if (!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
-        started = true;
+        started = true;       
     }
-
 });
 
 $(".btn").click(function (){ 
     var userChosenColour = $(this).attr("id"); //to refer to the button object that triggered the click; this.id also works
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
 }); 
 
 function nextSequence() {
+    userClickedPattern = [];
     level++;
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
@@ -44,9 +44,33 @@ function animatePress(currentColour) {
     setTimeout(function(){ $("#" + currentColour).removeClass("pressed"); }, 100);
 }
 
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log(currentLevel + gamePattern);
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else{
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
 
-
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 
 
 
